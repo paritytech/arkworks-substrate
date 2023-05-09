f1143737e29a34b23230d93aff4d668dbc8a584e
Achim Schneider <achim@parity.io>
2023-05-09 10:37:48 +0200

    impl ed_on_bls12_381_bandersnatch

--- ed_on_bls12_377/src/curves/tests.rs
+++ ed_on_bls12_377/src/curves/tests.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 use crate::EdwardsProjective;
-use crate::{EdwardsProjective as EdwardsProjectiveHost, HostFunctions};
 use ark_algebra_test_templates::*;
 
 test_group!(te; super::EdwardsProjective; te);
--- ed_on_bls12_381_bandersnatch/src/curves/mod.rs
+++ ed_on_bls12_381_bandersnatch/src/curves/mod.rs
@@ -1,1 +1,18 @@
+use sp_ark_ed_on_bls12_381_bandersnatch::{
+    BandersnatchConfig as BandersnatchConfigHost, EdwardsAffine as EdwardsAffineHost,
+    EdwardsConfig as EdwardsConfigHost, EdwardsProjective as EdwardsProjectiveHost,
+    SWAffine as SWAffineHost, SWConfig as SWConfigHost, SWProjective as SWProjectiveHost,
+};
 
+#[cfg(test)]
+mod tests;
+
+pub type EdwardsAffine = EdwardsAffineHost<crate::Host>;
+pub type EdwardsProjective = EdwardsProjectiveHost<crate::Host>;
+
+pub type SWAffine = SWAffineHost<crate::Host>;
+pub type SWProjective = SWProjectiveHost<crate::Host>;
+
+pub type SWConfig = SWConfigHost<crate::Host>;
+pub type EdwardsConfig = EdwardsConfigHost<crate::Host>;
+pub type BandersnatchConfig = BandersnatchConfigHost<crate::Host>;
