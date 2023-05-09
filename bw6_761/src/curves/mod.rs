bfa39d45e436a35703a200e4cacb82f0a4784d70
Achim Schneider <achim@parity.io>
2023-05-09 13:18:06 +0200

    add Config to bw6_761

--- bw6_761/src/curves/mod.rs
+++ bw6_761/src/curves/mod.rs
@@ -1,4 +1,4 @@
-use sp_ark_bw6_761::BW6_761 as BW6_761Host;
+use sp_ark_bw6_761::{BW6_761 as BW6_761Host, curves::Config as ConfigHost};
 
 pub mod g1;
 pub mod g2;
@@ -6,4 +6,5 @@ pub mod g2;
 #[cfg(test)]
 mod tests;
 
+pub type Config = ConfigHost<crate::Host>;
 pub type BW6_761 = BW6_761Host<crate::Host>;
